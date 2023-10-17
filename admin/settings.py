f17cdf034c8ebba6dad955387c7600faeaf9e79d
J-eris <erickxt@hotmail.com>
2023-10-17 17:44:01 -0600

    repair hosts

--- admin/settings.py
+++ admin/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-m^wp=b#u2nz)ztxjvj&ob@sr_rn7sd!p50))y+60xm7jz*ky@z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['143.110.236.190', '10.124.0.2']
 
 
 # Application definition
